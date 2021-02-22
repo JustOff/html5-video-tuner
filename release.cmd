b694ba03a0d2db15e4d9d7a8f9ef6ed43f983ac5
JustOff <Off.Just.Off@gmail.com>
2021-02-22 16:28:27 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.2.3
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/html5-media-tuner-.+?\.xpi/download\/%VER%\/html5-media-tuner-%VER%\.xpi/" update.xml
+
+set XPI=html5-media-tuner-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
